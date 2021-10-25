9660c56f2ec0f4a8c96a4330cb248cbbb278abd5
Waaiez Kinnear <43832467+Waaiez@users.noreply.github.com>
2021-10-25 12:01:25 +0200

    Create Navbar for medium screens and above
    Add Links to each Nav Item
    Add styling to Nav Items
    Add Anichart Logo

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Switch } from '@headlessui/react';
 import {
 	MenuIcon,
@@ -8,9 +8,37 @@ import {
 	CogIcon,
 } from '@heroicons/react/outline';
 import { FaRegSnowflake, FaSeedling, FaSun, FaLeaf } from 'react-icons/fa';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+function getSeason() {
+	let d = new Date();
+	let season = d.getMonth() + 1;
+	[10, 7, 4, 1].every((val) => {
+		if (season >= val) {
+			season = val;
+			return false;
+		}
+		return true;
+	});
+	if (season > 4) {
+		return d.getFullYear() + 1;
+	} else return d.getFullYear();
+}
 
 export default function Navbar() {
 	const [isShowing, setIsShowing] = useState(false);
+
+	const router = useRouter();
+
+	const [fullSeason, setFullSeason] = useState('');
+	const [path, setPath] = useState('');
+
+	useEffect(() => {
+		router.query.season && setFullSeason(router.query.season);
+		setPath(router.pathname.split('/')[1]);
+	});
 
 	return (
 		<>
@@ -19,63 +47,143 @@ export default function Navbar() {
 				{/* Menu 1 - seasons */}
 				{!isShowing && (
 					<div className='bg-[#2b2d42] h-14 w-full z-100 rounded-md mr-5 flex justify-evenly shadow-2xl sm-seasons'>
-						<div className='my-2 flex flex-col w-full'>
-							<FaRegSnowflake className='text-white h-5 w-5 flex mx-auto' />
-							<div className='text-xs text-center text-white'>
-								Winter
-							</div>
-						</div>
-						<div className='my-2 flex flex-col w-full'>
-							<FaSeedling className='text-white h-5 w-5 flex mx-auto' />
-							<div className='text-xs text-center text-white'>
-								Spring
-							</div>
-						</div>
-						<div className='my-2 flex flex-col w-full'>
-							<FaSun className='text-white h-5 w-5 flex mx-auto' />
-							<div className='text-xs text-center text-white'>
-								Summer
-							</div>
-						</div>
-						<div className='my-2 flex flex-col w-full'>
-							<FaLeaf className='text-white h-5 w-5 flex mx-auto' />
-							<div className='text-xs text-center text-white'>
-								Fall
-							</div>
-						</div>
+						<Link href={`/WINTER-${getSeason()}`}>
+							<a className='group'>
+								<div
+									className={`my-2 flex flex-col w-full group-hover:text-[#3AA0D8] transition duration-300 ease-in-out ${
+										fullSeason.split('-')[0] === 'WINTER'
+											? 'text-gray-100'
+											: 'text-gray-400'
+									}`}
+								>
+									<FaRegSnowflake className='h-5 w-5 flex mx-auto' />
+									<div className='text-xs text-center'>
+										Winter
+									</div>
+								</div>
+							</a>
+						</Link>
+						<Link href={`/SPRING-${getSeason()}`}>
+							<a className='group'>
+								<div
+									className={`my-2 flex flex-col w-full group-hover:text-[#3AA0D8] transition duration-300 ease-in-out ${
+										fullSeason.split('-')[0] === 'SPRING'
+											? 'text-gray-100'
+											: 'text-gray-400'
+									}`}
+								>
+									<FaSeedling className='h-5 w-5 flex mx-auto' />
+									<div className='text-xs text-center'>
+										Spring
+									</div>
+								</div>
+							</a>
+						</Link>
+						<Link href={`/SUMMER-${new Date().getFullYear()}`}>
+							<a className='group'>
+								<div
+									className={`my-2 flex flex-col w-full group-hover:text-[#3AA0D8] transition duration-300 ease-in-out ${
+										fullSeason.split('-')[0] === 'SUMMER'
+											? 'text-gray-100'
+											: 'text-gray-400'
+									}`}
+								>
+									<FaSun className='h-5 w-5 flex mx-auto' />
+									<div className='text-xs text-center'>
+										Summer
+									</div>
+								</div>
+							</a>
+						</Link>
+						<Link href={`/FALL-${new Date().getFullYear()}`}>
+							<a className='group'>
+								<div
+									className={`my-2 flex flex-col w-full group-hover:text-[#3AA0D8] transition duration-300 ease-in-out ${
+										fullSeason.split('-')[0] === 'FALL'
+											? 'text-gray-100'
+											: 'text-gray-400'
+									}`}
+								>
+									<FaLeaf className='h-5 w-5 flex mx-auto' />
+									<div className='text-xs text-center'>
+										Fall
+									</div>
+								</div>
+							</a>
+						</Link>
 					</div>
 				)}
 				{/* Menu 2 - charts */}
 				{isShowing && (
 					<div className='bg-[#2b2d42] h-14 w-full z-100 rounded-md mr-5 flex justify-evenly shadow-2xl sm-charts'>
-						<div className='my-2 flex flex-col w-full'>
-							<ArchiveIcon className='text-white h-5 w-5 flex mx-auto' />
-							<div className='text-xs text-center text-white'>
-								Archive
-							</div>
-						</div>
-						<div className='my-2 flex flex-col w-full'>
-							<ChevronDoubleRightIcon className='text-white h-5 w-5 flex mx-auto' />
-							<div className='text-xs text-center text-white'>
-								TBA
-							</div>
-						</div>
-						<div className='my-2 flex flex-col w-full'>
-							<CalendarIcon className='text-white h-5 w-5 flex mx-auto' />
-							<div className='text-xs text-center text-white'>
-								Airing
-							</div>
-						</div>
-						<div className='my-2 flex flex-col w-full'>
-							<CogIcon className='text-white h-5 w-5 flex mx-auto' />
-							<div className='text-xs text-center text-white'>
-								Settings
-							</div>
-						</div>
+						<Link href='/archive'>
+							<a className='group'>
+								<div
+									className={`my-2 flex flex-col w-full group-hover:text-[#3AA0D8] transition duration-300 ease-in-out ${
+										path === 'archive'
+											? 'text-gray-100'
+											: 'text-gray-400'
+									}`}
+								>
+									<ArchiveIcon className='h-5 w-5 flex mx-auto' />
+									<div className='text-xs text-center'>
+										Archive
+									</div>
+								</div>
+							</a>
+						</Link>
+						<Link href='/tba'>
+							<a className='group'>
+								<div
+									className={`my-2 flex flex-col w-full group-hover:text-[#3AA0D8] transition duration-300 ease-in-out ${
+										path === 'tba'
+											? 'text-gray-100'
+											: 'text-gray-400'
+									}`}
+								>
+									<ChevronDoubleRightIcon className='h-5 w-5 flex mx-auto' />
+									<div className='text-xs text-center'>
+										TBA
+									</div>
+								</div>
+							</a>
+						</Link>
+						<Link href='/airing'>
+							<a className='group'>
+								<div
+									className={`my-2 flex flex-col w-full group-hover:text-[#3AA0D8] transition duration-300 ease-in-out ${
+										path === 'airing'
+											? 'text-gray-100'
+											: 'text-gray-400'
+									}`}
+								>
+									<CalendarIcon className='h-5 w-5 flex mx-auto' />
+									<div className='text-xs text-center'>
+										Airing
+									</div>
+								</div>
+							</a>
+						</Link>
+						<Link href='/settings'>
+							<a className='group'>
+								<div
+									className={`my-2 flex flex-col w-full group-hover:text-[#3AA0D8] transition duration-300 ease-in-out ${
+										path === 'settings'
+											? 'text-gray-100'
+											: 'text-gray-400'
+									}`}
+								>
+									<CogIcon className='h-5 w-5 flex mx-auto' />
+									<div className='text-xs text-center'>
+										Settings
+									</div>
+								</div>
+							</a>
+						</Link>
 					</div>
 				)}
 
-				{/* Settings Icon */}
+				{/* Menu Icon */}
 				<Switch checked={isShowing} onChange={setIsShowing}>
 					<div className='bg-[#2b2d42] h-14 w-16 z-100 rounded-md flex items-center justify-center shadow-2xl sm-menu'>
 						<MenuIcon className='text-white h-10' />
@@ -84,6 +192,160 @@ export default function Navbar() {
 			</div>
 
 			{/* MD and above Navbar */}
+			<div className='hidden md:block'>
+				{/* Backround behind Navbar */}
+				<div className='bg-[#2b2d42] h-56 z-10 absolute inset-x-0 top-0'></div>
+				<div className='flex justify-between md:mx-10 lg:mx-16 my-10 h-11 navbar z-30 relative'>
+					{/* Logo */}
+					<div className='w-full flex justify-center anichartLogo'>
+						<Link href='/'>
+							<a>
+								<Image
+									src='/anichartLogo.svg'
+									alt='AnichartLogo'
+									width='45px'
+									height='45px'
+								/>
+							</a>
+						</Link>
+					</div>
+
+					<div className='w-full flex seasons text-center justify-center'>
+						<Link href={`/WINTER-${getSeason()}`}>
+							<a className='group'>
+								<div
+									className={`text-base font-bold w-full px-5 group-hover:text-gray-200 transition duration-300 ease-in-out ${
+										fullSeason.split('-')[0] === 'WINTER'
+											? 'text-gray-100'
+											: 'text-gray-400'
+									}`}
+								>
+									Winter
+								</div>
+								<div className='text-xs w-full px-5 text-gray-400'>
+									{getSeason()}
+								</div>
+							</a>
+						</Link>
+						<Link href={`/SPRING-${getSeason()}`}>
+							<a className='group'>
+								<div
+									className={`text-base font-bold w-full px-5 group-hover:text-gray-200 transition duration-300 ease-in-out ${
+										fullSeason.split('-')[0] === 'SPRING'
+											? 'text-gray-100'
+											: 'text-gray-400'
+									}`}
+								>
+									Spring
+								</div>
+								<div className='text-xs w-full px-5 text-gray-400'>
+									{getSeason()}
+								</div>
+							</a>
+						</Link>
+						<Link href={`/SUMMER-${new Date().getFullYear()}`}>
+							<a className='group'>
+								<div
+									className={`text-base font-bold w-full px-5 group-hover:text-gray-200 transition duration-300 ease-in-out ${
+										fullSeason.split('-')[0] === 'SUMMER'
+											? 'text-gray-100'
+											: 'text-gray-400'
+									}`}
+								>
+									Summer
+								</div>
+								<div className='text-xs w-full px-5 text-gray-400'>
+									{new Date().getFullYear()}
+								</div>
+							</a>
+						</Link>
+						<Link href={`/FALL-${new Date().getFullYear()}`}>
+							<a className='group'>
+								<div
+									className={`text-base font-bold w-full px-5 group-hover:text-gray-200 transition duration-300 ease-in-out ${
+										fullSeason.split('-')[0] === 'FALL'
+											? 'text-gray-100'
+											: 'text-gray-400'
+									}`}
+								>
+									Fall
+								</div>
+								<div className='text-xs w-full px-5 text-gray-400'>
+									{new Date().getFullYear()}
+								</div>
+							</a>
+						</Link>
+					</div>
+
+					{/* Charts */}
+					<div className='w-full flex charts text-center justify-center'>
+						<Link href='/airing'>
+							<a className='group'>
+								<div
+									className={`text-base font-bold w-full px-2 group-hover:text-gray-200 transition duration-300 ease-in-out ${
+										path === 'airing'
+											? 'text-gray-100'
+											: 'text-gray-400'
+									}`}
+								>
+									<CalendarIcon className='h-6 w-5 flex mx-auto' />
+								</div>
+								<div className='text-sm w-full px-2 text-gray-400'>
+									Airing
+								</div>
+							</a>
+						</Link>
+						<Link href='/archive'>
+							<a className='group'>
+								<div
+									className={`text-base font-bold w-full px-2 group-hover:text-gray-200 transition duration-300 ease-in-out ${
+										path === 'archive'
+											? 'text-gray-100'
+											: 'text-gray-400'
+									}`}
+								>
+									<ArchiveIcon className='h-6 w-5 flex mx-auto' />
+								</div>
+								<div className='text-sm w-full px-2 text-gray-400'>
+									Archive
+								</div>
+							</a>
+						</Link>
+						<Link href='/tba'>
+							<a className='group'>
+								<div
+									className={`text-base font-bold w-full px-2 group-hover:text-gray-200 transition duration-300 ease-in-out ${
+										path === 'tba'
+											? 'text-gray-100'
+											: 'text-gray-400'
+									}`}
+								>
+									<ChevronDoubleRightIcon className='h-6 w-5 flex mx-auto' />
+								</div>
+								<div className='text-sm w-full px-2 text-gray-400'>
+									TBA
+								</div>
+							</a>
+						</Link>
+						<Link href='/settings'>
+							<a className='group'>
+								<div
+									className={`text-base font-bold w-full px-2 group-hover:text-gray-200 transition duration-300 ease-in-out ${
+										path === 'settings'
+											? 'text-gray-100'
+											: 'text-gray-400'
+									}`}
+								>
+									<CogIcon className='h-6 w-5 flex mx-auto' />
+								</div>
+								<div className='text-sm w-full px-2 text-gray-400'>
+									Settings
+								</div>
+							</a>
+						</Link>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
