a951bf6b7dd166f6358e8290a7ed7e70c0711305
Waaiez Kinnear <43832467+Waaiez@users.noreply.github.com>
2021-10-25 16:42:38 +0200

    Start work on implementing themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        theme: {
+          base: 'var(--color-text-base)'
+        }
+      },
+      backgroundColor: {
+        theme: {
+          primary: 'var(--color-bg-primary)',
+          secondary: 'var(--color-bg-secondary)'
+        }
+      }
+    },
   },
   variants: {
     extend: {},
