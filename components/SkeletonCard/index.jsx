0bc3222f8b81a3dbf51fd5f61f503c003c405ebe
Waaiez Kinnear <43832467+Waaiez@users.noreply.github.com>
2021-10-28 20:03:51 +0200

    Skeleton Card fixes for mobile

--- components/SkeletonCard/index.jsx
+++ components/SkeletonCard/index.jsx
@@ -2,7 +2,7 @@ export default function SkeletonCard() {
 	return (
 		<div className=' max-h-72 min-w-96'>
 			<div className='h-72 rounded-md drop-shadow-xl border-2 border-gray-200 overflow-hidden bg-gray-100 flex'>
-				<div className='bg-gray-400 h-full w-4/5 animate-pulse'></div>
+				<div className='bg-gray-400 h-full xs:w-4/5 sm:w-2/4 w-1/3 lg:w-4/5 animate-pulse'></div>
 				<div className='flex flex-col w-full mx-4 mt-4'>
 					<p className='leading-relaxed mb-3 w-full h-7 animate-pulse bg-gray-400'></p>
 					<br />
